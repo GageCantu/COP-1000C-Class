8aba0230735307bf0e9216ecdafd18e6b35fa7ad
GageCantu <tcantu@valenciacollege.edu>
2024-09-09 12:30:04 -0400

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-#print statement to git
-print("Hello World")
+first = 5*6
+second = 8/3
+third = 8//3
+fourth = 8%3
+fifth = 4200000+23090
+sixth = 42000000-7633
+print("The answer to your query is", first)
+print("The answer to your query is", second)
+print("The answer to your query is", third)
+print("The answer to your query is", fourth)
+print("The answer to your query is", fifth)
+print("The answer to your query is", sixth)
